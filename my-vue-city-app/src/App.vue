f2f2ef5ffac28bebeaf32418a63f26838042c94a
zisuzin <zi_soo_zin@naver.com>
2023-05-08 12:09:06 +0900

    s

---
+++ my-vue-city-app/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <TopArea></TopArea>
+    <MainArea></MainArea>
+    <!-- <InfoArea></InfoArea> -->
+  </div>
+</template>
+
+<script>
+
+import TopArea from './components/TopComp.vue';
+import MainArea from './components/MainComp.vue';
+import InfoArea from './components/InfoComp.vue';
+
+export default {
+  name: 'App',
+  components: {
+    TopArea,
+    MainArea,
+    InfoArea
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
